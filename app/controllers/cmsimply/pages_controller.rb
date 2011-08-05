137c79bddb4312b7d037c230ce6d4927eaa1bdae
Nate Kidwell <nate@ludicast.com>
2011-08-05 10:49:32 -0400

    on way to friendlier layouts

--- app/controllers/cmsimply/pages_controller.rb
+++ app/controllers/cmsimply/pages_controller.rb
@@ -1,8 +1,9 @@
 module Cmsimply
 	class PagesController < ApplicationController
+		layout "application"
 		def show
 			params["page_name"] ||= "home"
-			render params["page_name"]
+			render params["page_name"], :layout => true
 		end
 	end
 end
